361f50a2bc82c9ea22241baaa22866554c99cddd
KotaSakurai <sakurai.kota@mwed.co.jp>
2017-06-27 19:14:36 +0900

    finish 1st

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,5 +1,14 @@
 require_relative 'message_filter'
 
-describe MessageFilter do
-  
+describe MessageFilter, 'with argument "foo"' do
+  # before do
+  #   @filter = MessageFilter.new('foo')
+  # end
+  # subject { @filter}
+
+  subject { MessageFilter.new('foo')}
+  it { is_expected.to be_detect('hello from foo') }
+    # expect(@filter).to be_detect('hello from foo')
+    #expect(@filter).to be_detect('hello from foo ')
+  it { is_expected.not_to be_detect('hello,world') }
 end
