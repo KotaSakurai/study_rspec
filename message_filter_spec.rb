658290f850354620e3996e8d8fd5fb26569f10e2
KotaSakurai <sakurai.kota@mwed.co.jp>
2017-06-28 15:23:50 +0900

    finish 2

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,14 +1,25 @@
 require_relative 'message_filter'
 
-describe MessageFilter, 'with argument "foo"' do
-  # before do
-  #   @filter = MessageFilter.new('foo')
-  # end
-  # subject { @filter}
-
-  subject { MessageFilter.new('foo')}
-  it { is_expected.to be_detect('hello from foo') }
+describe MessageFilter do
+  shared_examples 'MessageFilter with argument "foo"' do
     # expect(@filter).to be_detect('hello from foo')
     #expect(@filter).to be_detect('hello from foo ')
-  it { is_expected.not_to be_detect('hello,world') }
+    it { is_expected.to be_detect('hello from foo')}
+    it { is_expected.not_to be_detect('hello,world') }
+  end
+
+  context 'with argument "foo"' do
+    # before do
+    #   @filter = MessageFilter.new('foo')
+    # end
+    # subject { @filter}
+    subject { MessageFilter.new('foo')}
+    it_behaves_like 'MessageFilter with argument "foo"'
+  end
+
+  describe 'with argument "foo" , "bar" ' do
+    subject { MessageFilter.new('foo','bar')}
+    it { is_expected.to be_detect('hello from bar') }
+    it_behaves_like 'MessageFilter with argument "foo"'
+  end 
 end
