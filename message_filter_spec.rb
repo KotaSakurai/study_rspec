b9c916fdc3ca79c3e17dffd67fae2d044265a624
KotaSakurai <sakurai.kota@mwed.co.jp>
2017-06-29 10:45:43 +0900

    finish 3 iteration

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,4 +1,6 @@
 require_relative 'message_filter'
+require 'rspec/its'
+require 'rspec/collection_matchers'
 
 describe MessageFilter do
   shared_examples 'MessageFilter with argument "foo"' do
@@ -6,6 +8,7 @@ describe MessageFilter do
     #expect(@filter).to be_detect('hello from foo ')
     it { is_expected.to be_detect('hello from foo')}
     it { is_expected.not_to be_detect('hello,world') }
+    its(:ng_words) { is_expected.not_to be_empty }
   end
 
   context 'with argument "foo"' do
@@ -15,11 +18,20 @@ describe MessageFilter do
     # subject { @filter}
     subject { MessageFilter.new('foo')}
     it_behaves_like 'MessageFilter with argument "foo"'
+    # its(:ng_words) {
+    #   # expect(subject.ng_words.empty?).to eq false
+    #   expect(subject.ng_words).not_to be_empty
+    # }
+    its(:ng_words) {
+      # expect(subject.ng_words.size).to eq 1
+      is_expected.to have(1).ng_words
+     }
   end
 
-  describe 'with argument "foo" , "bar" ' do
+  context 'with argument "foo" , "bar" ' do
     subject { MessageFilter.new('foo','bar')}
     it { is_expected.to be_detect('hello from bar') }
     it_behaves_like 'MessageFilter with argument "foo"'
+    it { is_expected.to have(2).ng_words }
   end 
 end
